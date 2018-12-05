62699ea1118e57f33602d49da17050bde4719f0f
Patrick Pichler <patri.pichler@gmail.com>
2018-12-05 23:15:07 +0100

    Added second star solution for day 1

--- day1/day1.clj
+++ day1/day1.clj
@@ -2,11 +2,26 @@
 
 (require '[clojure.string :as string])
 
-(defn problem1 []
+
+(defn parse-input []
   (->> (slurp "input1")
        (string/split-lines)
-       (map #(Integer/parseInt %1))
-       (reduce +)))
+       (map #(Integer/parseInt %1))))
+
+(defn problem1 []
+       (reduce + (parse-input)))
+
+(defn problem2 []
+  (->> (parse-input)
+       (cycle)
+       (reduce (fn [[f fs] c] 
+                 (let 
+                   [nf (+ f c)]
+                      (if (contains? fs nf)
+                        (reduced nf)
+                        [nf (conj fs nf)])))
+            [0 #{}])))
 
 (defn -main [& args]
-  (println (problem1)))
+  (println (problem1))
+  (println (problem2)))
