a3c2e07e5935665d5d2eeba7ebde45f71b5823b8
Patrick Pichler <patri.pichler@gmail.com>
2018-12-07 09:22:01 +0100

    Added solution for part2 day2

--- day2/day2.clj
+++ day2/day2.clj
@@ -26,8 +26,27 @@
        (filter #(not= (first %1) (last %1)))
        (count)))
 
+(defn is-diff-by-one? [w1 w2]
+  (= (diff-by-word w1 w2) 1))
+
+(defn search-for-one-difference [id ids]
+  (filter (partial is-diff-by-one? id) ids))
+
+(defn get-equal-chars [w1 w2]
+  (->> (map vector w1 w2)
+       (filter #(= (first %1) (last %1)))
+       (map first)
+       (apply str)))
+
+(defn find-id-one-difference [id ids]
+  (let [[result] (search-for-one-difference id ids)]
+    (if (nil? result) (recur (first ids) (rest ids))
+      (get-equal-chars result id))))
+
 (defn problem2 []
-  )
+  (let [[head & tail] (parse-input)]
+    (find-id-one-difference head tail)))
 
 (defn -main [& args]
-  (println (problem1)))
+  (println (problem1))
+  (println (problem2)))
